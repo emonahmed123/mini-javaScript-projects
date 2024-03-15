b422a870a4c55e2427ca6f7c4206aa5f144bed65
Emon Ahmed <imonshomon@gmail.com>
2024-03-15 10:46:16 +0600

    done create withdraw input

--- bank/script.js
+++ bank/script.js
@@ -1,12 +1,12 @@
-document.getElementById('button-sub').addEventListener('click', function () {
-
+document.getElementById('button-sub').addEventListener('click', function (e) {
+    e.preventDefault()
     const emailvalue = document.getElementById('user-email');
     const email = emailvalue.value
     const passwordFild = document.getElementById('user-password')
     const password = passwordFild.value
     console.log(email)
 
-    if (email === 'imon@.com' && password === '123') {
+    if (email === 'imon@gmail.com' && password === '123') {
         window.location.href = 'bank.html'
     }
     else {
