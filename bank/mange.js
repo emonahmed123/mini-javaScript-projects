882c664edc05e50f56934b594da40d1318995706
Emon Ahmed <imonshomon@gmail.com>
2024-03-15 11:56:25 +0600

    done deposite

---
+++ bank/mange.js
@@ -0,0 +1,37 @@
+
+document.getElementById('button-deposit').addEventListener('click', function (e) {
+    e.preventDefault()
+    const depositField = document.getElementById('deposit-feild')
+
+    const NewdepositAmountString = depositField.value;
+    const NewdepositAmountNum = parseFloat(NewdepositAmountString)
+
+    const depositTotalElement = document.getElementById('deposit-total')
+
+
+    const PreveousDepostiTotal = parseFloat(depositTotalElement.innerText);
+    const cureenTotal = PreveousDepostiTotal + NewdepositAmountNum
+    depositTotalElement.innerText = cureenTotal
+
+
+    //  total blance
+
+    const totalBlance = document.getElementById('total-blance')
+    const previesTotalString = totalBlance.innerText
+    const previesTotal = parseFloat(previesTotalString)
+
+    const newTotal = previesTotal + NewdepositAmountNum
+
+    totalBlance.innerText = newTotal
+
+
+
+    depositField.value = ''
+
+
+
+
+
+
+
+})
