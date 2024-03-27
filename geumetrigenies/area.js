2c2342e6a43061a9961c7ac999cd37385e109324
Emon Ahmed <imonshomon@gmail.com>
2024-03-27 10:38:08 +0600

    done

--- geumetrigenies/area.js
+++ geumetrigenies/area.js
@@ -18,3 +18,23 @@ function calculateArea() {
     triangelArea.innerText = area
 
 }
+function calculateRectangleArea() {
+    const widthField = document.getElementById('Rectangle-width');
+    const widthValueText = widthField.value
+    const width = parseFloat(widthValueText)
+
+
+    // get triangel height value;
+
+    const lengthField = document.getElementById('rectangle-length');
+    const lengthValueText = lengthField.value;
+    const length = parseFloat(lengthValueText)
+
+
+    const area = width * length;
+
+    const reaiangelArea = document.getElementById('rectangle-area');
+
+    reaiangelArea.innerText = area
+
+}
